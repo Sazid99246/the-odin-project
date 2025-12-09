a21caab8e36432fb250da8daf3f314af330fcdbf
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2025-12-09 13:10:48 +0600

    object exercise completed

---
+++ Full Stack JavaScript/JavaScript/object-exercises/book.js
@@ -0,0 +1,17 @@
+function Book(title, author, pages, read) {
+    if (!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor");
+    }
+
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+
+    this.info = function () {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+    }
+}
+
+const hobbit = new Book("The Hobbit", "J.R.R", 295, "not read yet");
+console.log(hobbit.info());
