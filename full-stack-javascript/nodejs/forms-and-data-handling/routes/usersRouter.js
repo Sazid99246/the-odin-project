87b205277a66a9e8d56f4d8c0d0c05704e2e3014
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2026-01-03 21:50:30 +0600

    all directories renamed

---
+++ full-stack-javascript/nodejs/forms-and-data-handling/routes/usersRouter.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+const usersController = require("../controllers/usersController");
+const usersRouter = Router();
+
+usersRouter.get("/", usersController.usersListGet);
+usersRouter.get("/create", usersController.usersCreateGet);
+usersRouter.post("/create", usersController.usersCreatePost);
+usersRouter.post("/:id/delete", usersController.usersDeletePost);
+
+module.exports = usersRouter;
